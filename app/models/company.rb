eb37cbc12339bc79f27156f031f8612074ad482c
Axelut Alex <Axelut@Axeluts-MacBook-Pro.local>
2013-07-01 16:14:42 +0300

    add javascript

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,13 @@
 class Company < ActiveRecord::Base
   attr_accessible :address, :city, :country, :county, :name, :zip_code
   
- has_many :users, :foreign_key => "company_id"
- has_many :customers
- has_many :invoices
- has_many :transactions
+  has_many :users, :foreign_key => "company_id"
+  has_many :customers
+  has_many :invoices
+  has_many :transactions
   
+  validates :name, :address, :city, :country, :zip_code, :presence => true
+  validates :name, :length => { :minimum => 2 }
+  validates_uniqueness_of :name
+   
 end
