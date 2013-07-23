cdee233915ebac8ff3da3975ec08f4c5a7c1ed93
conacelelena <conacel.elena@gmail.com>
2013-07-23 15:50:47 +0300

    rows click-able

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130716130710) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "actions", force: true do |t|
     t.datetime "sent_at"
     t.datetime "viewed_at"
